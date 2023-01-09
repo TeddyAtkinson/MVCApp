c31cd729c5a044b90c250e2025831ddde2298e27
teddyatkinson <tedddy.atkinson@gmail.com>
2023-01-08 23:14:34 -0800

    Refactor Vendor Controller naming

--- MVCApp/Controllers/VendorController.cs
+++ MVCApp/Controllers/VendorController.cs
@@ -7,27 +7,27 @@ namespace MVCApp.Controllers
 {
   public class VendorsController : Controller
   {
-    [HttpGet("/Vendors")]
+    [HttpGet("/vendors")]
     public ActionResult Index()
     {
-      List<Vendor> allVendors = Vendor.GetAll();
-      return View(allVendors);
+      List<Vendor> allvendors = Vendor.GetAll();
+      return View(allvendors);
     }
 
-    [HttpGet("/Vendors/Create")]
-    public ActionResult Create()
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
     {
       return View();
     }
 
-    [HttpPost("/Vendors")]
+    [HttpPost("/vendors")]
     public ActionResult Create(string name, string description)
     {
       Vendor myVendor = new Vendor(name, description);
       return RedirectToAction("Index");
     }
     
-    [HttpGet("/Vendors/{id}")]
+    [HttpGet("/vendors/{id}")]
     public ActionResult Show(int id)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
@@ -39,11 +39,11 @@ namespace MVCApp.Controllers
     }
 
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderTitle, string orderDate, string orderDescription, int orderQuantity, int orderPrice)
+    public ActionResult Create(int vendorId, string orderTitle, string orderDate, string orderDescription, int orderPrice)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderTitle, orderDate, orderDescription, orderQuantity, orderPrice);
+      Order newOrder = new Order(orderTitle, orderDate, orderDescription, orderPrice);
       foundVendor.AddOrder(newOrder);
       List<Order> categoryOrders = foundVendor.Orders;
       model.Add("orders", categoryOrders);
